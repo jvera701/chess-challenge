7bcd0c81bf79af22e2fdfe4c0a9c34faee22942f
Jose Alejandro Vera <javera701@gmail.com>
2023-07-19 11:18:26 -0500

    Updates

---
+++ precache-manifest.ede8863ea629c4b7e5a8d894e448e2a0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0aa58a15210d2e23ed76",
+    "url": "/chess-challenge/static/css/main.a1d463e2.chunk.css"
+  },
+  {
+    "revision": "0aa58a15210d2e23ed76",
+    "url": "/chess-challenge/static/js/main.0aa58a15.chunk.js"
+  },
+  {
+    "revision": "444f0bc38a785630c6eb",
+    "url": "/chess-challenge/static/js/1.444f0bc3.chunk.js"
+  },
+  {
+    "revision": "ff813772de7efb309245",
+    "url": "/chess-challenge/static/js/2.ff813772.chunk.js"
+  },
+  {
+    "revision": "b57e3d51a3c38689b31d",
+    "url": "/chess-challenge/static/js/runtime~main.b57e3d51.js"
+  },
+  {
+    "revision": "7cf0ae40646eb73f01aa42a5be0ca4f8",
+    "url": "/chess-challenge/index.html"
+  }
+];
